9f8a27bb25dd2aed9a06d70e9418cac8976f1d95
Pedro Del Gallego <pedro.delgallego@gmail.com>
2011-02-04 00:46:57 +0100

    Add init method to pema framework

--- example/pema-contact/app/observers/location_jquery_observer.js
+++ example/pema-contact/app/observers/location_jquery_observer.js
@@ -4,16 +4,15 @@
       route: pema.hash_path(),
       id: matches[1]
     }); 
-  }); 
+  });
 
   pema.observer.addRoute("#/contacts/edit/([0-9]*)", function(matches){
     $( pema.observer ).trigger( "#/contacts/edit/:id", { 
       route: pema.hash_path(),
       id: matches[1]
     }); 
-  }); 
+  });
 
-  pema.instance('contactsController').init(); 
-  pema.instance('contentController').init(); 
-  pema.observer.init();
+  pema.init();
+
 }) ()
